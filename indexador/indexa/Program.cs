1422f60716989f2e806264d3dcd296d27b34724a
rubenguzman48 <rubenguzman48@gmail.com>
2023-06-28 00:03:30 -0300

    1er commit indexado

---
+++ indexador/indexa/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        string path;
+        Console.Write("Ingrese una ruta valida: ");
+        path = Console.ReadLine();
+
+        if (!Directory.Exists(path))
+        {
+            Console.WriteLine("Ruta invalida!");
+        }else
+        {
+            List<string> listarArchivos = Directory.GetFiles(path).ToList();
+            Console.WriteLine("Lista de archivos");
+            listarArchivos.ForEach(Console.WriteLine); //en esta linea imprimo cada elemento de la lista por consola
+            using(StreamWriter indexador = new StreamWriter("index.csv"))
+            {
+                for (int i = 0; i < listarArchivos.Count; i++)
+                {
+                    indexador.WriteLine($"{i},{Path.GetFileNameWithoutExtension(listarArchivos[i])},{Path.GetExtension(listarArchivos[i])}");
+                }
+                indexador.Close();
+                indexador.Dispose();
+            }
+
+
+        }
+    }
+}
